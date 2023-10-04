0c7653218bd3bad9e438dc633b6d46f326fc23ef
wettestsock <evilsharkeater@gmail.com>
2023-10-03 20:49:36 -0400

    Add project files.

---
+++ cppStats.cpp
@@ -0,0 +1,12 @@
+﻿// cppStats.cpp : Defines the entry point for the application.
+//
+
+#include "cppStats.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
